d5803abdf1115ed621524980e49b2cebdf07b61a
MaferT05 <mafer0506mejito@gmail.com>
2025-02-05 22:48:57 -0600

    Update ProyectoFinal.java

--- ProyectoFinal.java
+++ ProyectoFinal.java
@@ -1,1 +1,17 @@
-import java.swing.*;
+import javax.swing.*;
+
+public class ProyectoFinal {
+public static void main(String[] args) {
+JFrame frame = new JFrame("Mi primera Ventana");
+  frame.setSize(600, 300);
+  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+  frame.setLayout(null);
+
+  JLabel etiqueta_anuncio = new JLabel(" ¡Hola, somos Mafer y Sam! ");
+  JLabel etiqueta_2 = new JLabel("Prueba");
+  JLabel etiqueta_3 = new JLabel("Prueba encima 1");
+  JLabel etiqueta_4 = new JLabel("Prueba encima 2");
+  JButton button = new JButton ("Enviar");
+  JLabel nombre_usuario = new  JLabel ("Nombre de Usuario");
+  JTextField textField = new JTextField(20);
