b8cf84e6c6f9c09d27728bc5012475da7cd31630
Samantha1401 <martinezsami410@gmail.com>
2025-02-06 00:02:52 -0600

    Update ProyectoFinal.java

--- ProyectoFinal.java
+++ ProyectoFinal.java
@@ -16,4 +16,24 @@ JFrame frame = new JFrame("Mi primera Ventana");
   JLabel nombre_usuario = new  JLabel ("Nombre de Usuario");
   JTextField textField = new JTextField(20);
 
+  etiqueta_anuncio.setBounds(30, 50, 100, 30); 
+  etiqueta_2.setBounds(300, 50, 200, 30);
+  etiqueta_3.setBounds(30,90, 100,30);
+  etiqueta_4.setBounds( 300, 90, 150, 30);
+  button.setBounds(100, 120, 150, 30); 
+  nombre_usuario.setBounds(100, 180, 200, 30);
+  textField.setBounds(200, 160, 200, 30);
+
+  // Agregar los componentes al JFrame
+  frame.add(etiqueta_anuncio);
+  frame.add(etiqueta_2);
+  frame.add(etiqueta_3);
+  frame.add(etiqueta_4);
+  frame.add(button);
+  frame.add(nombre_usuario);
+
+  // Hacer visible la ventana
+  frame.setVisible(true);
+  }
+
   
